d08a1e0a54f39e15089a9329d513964f81bb4f08
Allan Clark <allanc@chickenandporn.com>
2014-06-05 14:16:41 -0700

    clean up documentation

--- classorg_1_1smallfoot_1_1vw4_1_1VWImport_1_1Entity.js
+++ classorg_1_1smallfoot_1_1vw4_1_1VWImport_1_1Entity.js
@@ -1,8 +1,7 @@
 var classorg_1_1smallfoot_1_1vw4_1_1VWImport_1_1Entity =
 [
-    [ "getitl_patterns", "classorg_1_1smallfoot_1_1vw4_1_1VWImport_1_1Entity.html#ad56904318d0b6b82c74f21c180c20c86", null ],
-    [ "gettags", "classorg_1_1smallfoot_1_1vw4_1_1VWImport_1_1Entity.html#a7bd476e2ab1263de941e590ea1e6e97c", null ],
     [ "itl_patterns", "classorg_1_1smallfoot_1_1vw4_1_1VWImport_1_1Entity.html#abf0e0c927e408646f190445003c49a60", null ],
+    [ "tags", "classorg_1_1smallfoot_1_1vw4_1_1VWImport_1_1Entity.html#abd918c8d92bd34f633ea27e3f5d43b3c", null ],
     [ "description", "classorg_1_1smallfoot_1_1vw4_1_1VWImport_1_1Entity.html#a76d2b0133d83c43dfd8a19286ac55325", null ],
     [ "edit_type", "classorg_1_1smallfoot_1_1vw4_1_1VWImport_1_1Entity.html#a76dab48266f34c941c947433ae14176d", null ],
     [ "itl_patterns", "classorg_1_1smallfoot_1_1vw4_1_1VWImport_1_1Entity.html#a88540be4d74409418ead7a4a7bdc8cee", null ],
--- classorg_1_1smallfoot_1_1vw4_1_1VirtualWisdom4ClientTool.js
+++ classorg_1_1smallfoot_1_1vw4_1_1VirtualWisdom4ClientTool.js
@@ -2,12 +2,11 @@ var classorg_1_1smallfoot_1_1vw4_1_1VirtualWisdom4ClientTool =
 [
     [ "VirtualWisdom4ClientTool", "classorg_1_1smallfoot_1_1vw4_1_1VirtualWisdom4ClientTool.html#a0f1b4c518fafff8a296bb632e57655ca", null ],
     [ "VirtualWisdom4ClientTool", "classorg_1_1smallfoot_1_1vw4_1_1VirtualWisdom4ClientTool.html#a7991b30b52ad4d408df3575abc9b57ae", null ],
-    [ "_load", "classorg_1_1smallfoot_1_1vw4_1_1VirtualWisdom4ClientTool.html#adc3a8f6eb0246c78615de6cc346d4f99", null ],
-    [ "_save", "classorg_1_1smallfoot_1_1vw4_1_1VirtualWisdom4ClientTool.html#adb7f600accd4a29f376ce8e52edff11a", null ],
-    [ "load", "classorg_1_1smallfoot_1_1vw4_1_1VirtualWisdom4ClientTool.html#a06e5248d8619f00173d9794f30966dc3", null ],
-    [ "save", "classorg_1_1smallfoot_1_1vw4_1_1VirtualWisdom4ClientTool.html#aa9370fe3a8b0b2d5b91a76190c1599e0", null ],
-    [ "search", "classorg_1_1smallfoot_1_1vw4_1_1VirtualWisdom4ClientTool.html#af1af4b7f062c6dd322b7e7a81209afb6", null ],
-    [ "setAttr", "classorg_1_1smallfoot_1_1vw4_1_1VirtualWisdom4ClientTool.html#aef43485334ab87e6db498fe403162255", null ],
-    [ "subAttr", "classorg_1_1smallfoot_1_1vw4_1_1VirtualWisdom4ClientTool.html#a2e34675797f629121c1b2a74a19ebd34", null ],
+    [ "_load", "classorg_1_1smallfoot_1_1vw4_1_1VirtualWisdom4ClientTool.html#ad9a051ba608e7fcb9adac39bc3946058", null ],
+    [ "_save", "classorg_1_1smallfoot_1_1vw4_1_1VirtualWisdom4ClientTool.html#a36a7decd28b5e191bfe43c5562462785", null ],
+    [ "load", "classorg_1_1smallfoot_1_1vw4_1_1VirtualWisdom4ClientTool.html#a0d686f1044a2e8727b12e6e4921e0e8f", null ],
+    [ "main", "classorg_1_1smallfoot_1_1vw4_1_1VirtualWisdom4ClientTool.html#a75988cf84fc6ee7a2ebff36e363021aa", null ],
+    [ "prettyJSON", "classorg_1_1smallfoot_1_1vw4_1_1VirtualWisdom4ClientTool.html#afc3ee9b897e0757b6cd5c8289c9d4cc4", null ],
+    [ "save", "classorg_1_1smallfoot_1_1vw4_1_1VirtualWisdom4ClientTool.html#a119573b242d96a5e64a7d340bcf14aa8", null ],
     [ "xmlDocument", "classorg_1_1smallfoot_1_1vw4_1_1VirtualWisdom4ClientTool.html#ad25ef6220eb54575157ab063bc63a0f0", null ]
 ];
