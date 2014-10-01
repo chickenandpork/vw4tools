ec8818d341c85858682a78dcaa8a67e496c517cd
Allan Clark <allanc@chickenandporn.com>
2014-10-01 09:17:35 -0700

    version-bump to 1.0 and ensure version mismatches are henceforth fatal

--- classorg_1_1smallfoot_1_1vw4_1_1VirtualWisdom4ClientTool.js
+++ classorg_1_1smallfoot_1_1vw4_1_1VirtualWisdom4ClientTool.js
@@ -5,6 +5,7 @@ var classorg_1_1smallfoot_1_1vw4_1_1VirtualWisdom4ClientTool =
     [ "VirtualWisdom4ClientTool", "classorg_1_1smallfoot_1_1vw4_1_1VirtualWisdom4ClientTool.html#a7991b30b52ad4d408df3575abc9b57ae", null ],
     [ "_load", "classorg_1_1smallfoot_1_1vw4_1_1VirtualWisdom4ClientTool.html#ad9a051ba608e7fcb9adac39bc3946058", null ],
     [ "_save", "classorg_1_1smallfoot_1_1vw4_1_1VirtualWisdom4ClientTool.html#a36a7decd28b5e191bfe43c5562462785", null ],
+    [ "check", "classorg_1_1smallfoot_1_1vw4_1_1VirtualWisdom4ClientTool.html#a6ffcb3b58a6bcea538f374321414a141", null ],
     [ "entities", "classorg_1_1smallfoot_1_1vw4_1_1VirtualWisdom4ClientTool.html#aa9ab77e799e869cf9da3d339e124f6c4", null ],
     [ "load", "classorg_1_1smallfoot_1_1vw4_1_1VirtualWisdom4ClientTool.html#a0d686f1044a2e8727b12e6e4921e0e8f", null ],
     [ "loadAndAbsorbFile", "classorg_1_1smallfoot_1_1vw4_1_1VirtualWisdom4ClientTool.html#a36539eb2d98fcdcedd7dc2088acfeef2", null ],
